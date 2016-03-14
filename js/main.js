b1643073846cb8e137c1379b498c0cbe0b7a7cee
Jorge Monroy <jlmonroy13@gmail.com>
2016-03-13 21:05:41 -0500

    resposive home

--- js/main.js
+++ js/main.js
@@ -2,5 +2,13 @@ $(function(){
 	$('.navbar-toggle').on('click', function(){
 		$('.navbar').toggleClass('navbar-collap');
 	});
+	$('.img-container').on('mouseenter', function(){
+		$(this).children('.opacity-color').show();
+		$(this).children('.product-more').show();
+	});
+	$('.img-container').on('mouseleave', function(){
+		$(this).children('.opacity-color').hide();
+		$(this).children('.product-more').hide();
+	});
 });
 	
